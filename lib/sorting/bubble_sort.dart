0fe2dab35a23ce453a469f20078df824f5f8eeb5
Timilehin Jegede <timilehintimothy.jegede@gmail.com>
2022-11-18 17:56:51 +0100

    chore: fixed bubble sort first iteration.

--- lib/sorting/bubble_sort.dart
+++ lib/sorting/bubble_sort.dart
@@ -3,7 +3,7 @@ import 'package:dart_algorithms/sorting/swapper.dart';
 void bubbleSort<E extends Comparable>(List<E> list) {
   final n = list.length;
 
-  for (var i = 0; i < n; i++) {
+  for (var i = 0; i < n - 1; i++) {
     var swapped = false;
 
     for (var j = 0; j < n - 1 - i; j++) {
